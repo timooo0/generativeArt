95538cfa725fc2bdf72436ac6c9ea3a2a39d20e5
timooo0@hotmail.com <36dAF5VchEsS>
2024-08-16 13:14:25 +0200

    Optimize Connections

--- assets/js/Connections.js
+++ assets/js/Connections.js
@@ -1,4 +1,4 @@
-let nCircles = 75;
+let nCircles;
 let canvasX;
 let canvasY;
 let radius = 10;
@@ -6,6 +6,7 @@ let posX = Math.random()*canvasX;
 let posY = Math.random()*canvasY;
 let distance2;
 let sortedArray;
+let invNorm = -1;
 const positions = [];
 const velocity = [];
 const neighbours = [];
@@ -15,12 +16,13 @@ function setup() {
     canvasY = window.innerHeight*0.8;
     let sketchCanvas = createCanvas(canvasX,canvasY);
     sketchCanvas.parent("scriptDiv");
-
+    nCircles = Math.floor(canvasX*canvasY/7000);
+    console.log("nCircles", nCircles);
     frameRate(30);
     for (let i=0; i< nCircles;i++) {
         positions.push([radius+Math.random()*(canvasX-2*radius), radius+Math.random()*(canvasY-2*radius)]);
         velocity.push([(Math.random()-0.5)*25, (Math.random()-0.5)*25]);
-        neighbours.push([])
+        neighbours.push(0)
 
     }
 }
@@ -29,19 +31,19 @@ function draw() {
     clear();
     background(10);
     for (let i=0; i < nCircles;i++) {
-        neighbours[i] = []
+        neighbours[i] = 0
     }
     for (let i=0; i < nCircles;i++) {
+        stroke(340);
         for (let j=i; j < nCircles;j++) {
             if (i!=j) {
                 // Radius around particle
                 if ((positions[i][0]-positions[j][0])**2 + (positions[i][1]-positions[j][1])**2 < 10000){
-                    if (!neighbours[i].includes(j)){
-                        stroke(340);
+                    // if (!neighbours[i].includes(j)){
                         line(positions[i][0],positions[i][1], positions[j][0], positions[j][1]);
-                        neighbours[i].push(j);
-                        neighbours[j].push(i);
-                    }
+                        neighbours[i] += 1;
+                        neighbours[j] += 1;
+                    // }
                 }
                         
                 // n nearest neighbours
@@ -61,13 +63,28 @@ function draw() {
         //     }
         // } 
 
-        drawingContext.shadowBlur = 10+Math.min(neighbours[i].length, 10)*10;
-        drawingContext.shadowColor = color(50+Math.min(neighbours[i].length, 5)*40);
-        fill(50+Math.min(neighbours[i].length, 10)*20);
+        // drawingContext.shadowBlur = 10+Math.min(neighbours[i], 10)*10;
+        // drawingContext.shadowBlur = 80;
+        // drawingContext.shadowColor = color(250);
+        // drawingContext.filter = 'blur(1px)'
+        noFill();
+        strokeWeight(1);
+        let opacity = 1;
+        let brightness = 50+Math.min(neighbours[i], 10)*20;
+        for(let z = 0; z<(brightness-50)/20; z++){
+            stroke('rgba('+brightness+','+brightness+','+brightness+', '+opacity + ')');
+            ellipse(positions[i][0], positions[i][1], radius+2*(z+0), radius+2*(z+0));
+            opacity /= 1.5;
+        }
+        strokeWeight(1.5);
         noStroke();
+        fill(brightness);
         ellipse(positions[i][0], positions[i][1], radius, radius);
-        positions[i][0] += velocity[i][0]/(1+2*Math.sqrt(neighbours[i].length));
-        positions[i][1] += velocity[i][1]/(1+2*Math.sqrt(neighbours[i].length));
+
+
+        invNorm = 1/(1+2*Math.sqrt(neighbours[i]))
+        positions[i][0] += velocity[i][0]*invNorm;
+        positions[i][1] += velocity[i][1]*invNorm;
         if (positions[i][0] + radius > canvasX || positions[i][0] - radius < 0) {
             velocity[i][0] *= -1
         }
