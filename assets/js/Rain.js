a415539573fa6bde45264d14a9e949dbc4c65a4e
timooo0@hotmail.com <36dAF5VchEsS>
2024-08-31 16:30:01 +0200

    TOTOROO

--- assets/js/Rain.js
+++ assets/js/Rain.js
@@ -8,7 +8,6 @@ let rainColor1;
 let rainColor2;
 let rainWidth = 3;
 let blockers = []; // Start X, radius, Y offset, walking speed, walking height
-let myShader;
 
 function setup() {
   let canvasDiv = document.getElementById("scriptDiv");
@@ -18,107 +17,165 @@ function setup() {
   sketchCanvas.parent("scriptDiv");
 
   frameRate(60);
-  rainColor1 = color('rgba(150,0,250,0.3)');
-  rainColor2 = color('rgba(150,100,250,0.1)');
+  rainColor1 = color("rgba(150,0,250,0.3)");
+  rainColor2 = color("rgba(150,100,250,0.1)");
   background(20);
-  for (let i=0;i<canvasX*rainDensity;i++){
-    for (let j=0;j<canvasY*rainDensity;j++){
-      rain.push([Math.random()*canvasX, // x position
-         Math.random()*canvasY, // y position
-        rainBaseLength*(Math.random()-0.5)*1.5]); // length
+  for (let i = 0; i < canvasX * rainDensity; i++) {
+    for (let j = 0; j < canvasY * rainDensity; j++) {
+      rain.push([
+        Math.random() * canvasX, // x position
+        Math.random() * canvasY, // y position
+        rainBaseLength * (Math.random() - 0.5) * 1.5, // length
+      ]); 
     }
   }
 }
 
 function drawRain() {
-  for (let i=0;i<rain.length;i++){
-
+  for (let i = 0; i < rain.length; i++) {
     stroke(rainColor1);
-    for (const blocker of blockers){
-      if (rain[i][0] > blocker[0] && rain[i][0] < blocker[0]+2*blocker[1]){
-        if (rain[i][1]+rain[i][2] > -0.5*Math.sqrt(blocker[1]**2 - ((rain[i][0]-blocker[0]-blocker[1]))**2)+blocker[2]){
+    for (const blocker of blockers) {
+      if (rain[i][0] > blocker[0] && rain[i][0] < blocker[0] + 2 * blocker[1]) {
+        if (
+          rain[i][1] + rain[i][2] >
+          -0.5 *
+            Math.sqrt(
+              blocker[1] ** 2 - (rain[i][0] - blocker[0] - blocker[1]) ** 2
+            ) +
+            blocker[2]
+        ) {
           stroke(rainColor2);
         }
       }
     }
 
-    line(rain[i][0], rain[i][1], rain[i][0], rain[i][1]+rain[i][2]);
+    line(rain[i][0], rain[i][1], rain[i][0], rain[i][1] + rain[i][2]);
   }
 }
 
-function updateRain(){
-  for (let i=0;i<rain.length;i++){
-    rain[i][1] += rain[i][2]*0.1+fallDistance;
+function updateRain() {
+  for (let i = 0; i < rain.length; i++) {
+    rain[i][1] += rain[i][2] * 0.1 + fallDistance;
     if (rain[i][1] > canvasY) {
       rain[i][1] = 0;
-      rain[i][0] = Math.random()*canvasX;
+      rain[i][0] = Math.random() * canvasX;
     }
   }
 }
 
-function updateObjects(){
-  if (blockers.length < 1 ) {
-    let radius = Math.random()*50+75
-    let speed = 1+Math.random()*2;
-    if (Math.random() < 100000/(60*3)){
-      blockers.push([
-        -2*radius,
-        radius,
-        0,
-        speed,
-        canvasY-3*radius
-      ]);
-      
-      //small 
+function updateObjects() {
+  if (blockers.length < 1) {
+    let radius = Math.random() * 50 + 75;
+    let speed = 1 + Math.random() * 2;
+    if (Math.random() < 100000 / (60 * 3)) {
+      blockers.push([-2 * radius, radius, 0, speed, canvasY - 3 * radius]);
+
+      //small
       if (Math.random() < 0.3) {
-        let smallRadius = Math.random()*20+50
+        let smallRadius = Math.random() * 20 + 50;
         blockers.push([
-          -4*smallRadius-2*radius,
+          -4 * smallRadius - 2 * radius,
           smallRadius,
           0,
           speed,
-          canvasY-3*smallRadius
+          canvasY - 3 * smallRadius,
         ]);
       }
     }
   }
 
   // Update the location
-  for (const blocker of blockers){
+  for (const blocker of blockers) {
     blocker[0] += blocker[3];
-    blocker[2] = blocker[3]*10*Math.cos(blocker[0]*Math.PI/(canvasX/10))**2+blocker[4];
+    blocker[2] =
+      0.5 *
+        blocker[1] * (Math.abs(Math.cos((blocker[0] * Math.PI) / (canvasX / ((10 * 75) / blocker[1])))) - 1) + blocker[4];
   }
 
   // Remove any that are out of frame
-  console.log(blockers)
-  blockers = blockers.filter(blocker => blocker[0] < canvasX);
+  blockers = blockers.filter((blocker) => blocker[0] < canvasX);
 }
 
 function drawPerson() {
-  for (const blocker of blockers){
-    let c = color('rgb(0,0,30)');
+  for (const blocker of blockers) {
+    let c = color("rgb(0,0,30)");
     noStroke();
     fill(c);
-    arc(blocker[0]+blocker[1], blocker[2], 2*blocker[1], 0.5*2*blocker[1], PI, 0);
-    
+    // Umbrella top
+    arc(
+      blocker[0] + blocker[1],
+      blocker[2],
+      2 * blocker[1],
+      0.5 * 2 * blocker[1],
+      PI,
+      0
+    );
+
     stroke(c);
-    line(blocker[0]+blocker[1], blocker[2], blocker[0]+blocker[1], blocker[2]+blocker[1]);
+    // Connection rod
+    line(
+      blocker[0] + blocker[1],
+      blocker[2],
+      blocker[0] + blocker[1],
+      blocker[2] + blocker[1]
+    );
 
     noFill();
-    strokeWeight(rainWidth*2);
-    line(blocker[0]+blocker[1], blocker[2]+0.8*blocker[1], blocker[0]+blocker[1], blocker[2]+blocker[1])
-    arc(blocker[0]+blocker[1]+0.1*blocker[1], blocker[2]+blocker[1], 0.2*blocker[1], 0.2*blocker[1], 0, PI);
+    strokeWeight(rainWidth * 2);
+    // Handle
+    line(
+      blocker[0] + blocker[1],
+      blocker[2] + 0.8 * blocker[1],
+      blocker[0] + blocker[1],
+      blocker[2] + blocker[1]
+    );
+    arc(
+      blocker[0] + blocker[1] + 0.1 * blocker[1],
+      blocker[2] + blocker[1],
+      0.2 * blocker[1],
+      0.2 * blocker[1],
+      0,
+      PI
+    );
+
+    // Bro
+    noStroke();
+    fill(c);
+    ellipse(
+      blocker[0] + 0.75 * blocker[1],
+      blocker[2] + 0.25 * blocker[1],
+      0.25 * blocker[1],
+      0.9 * blocker[1]
+    );
+
+    ellipse(
+      blocker[0] + 0.9 * blocker[1],
+      blocker[2] + 1.3 * blocker[1],
+      1.25 * blocker[1],
+      1.5 * blocker[1]
+    );
+
+    ellipse(
+      blocker[0] + 1.3 * blocker[1],
+      blocker[2] + 1.15 * blocker[1],
+      0.8 * blocker[1],
+      0.5 * blocker[1]
+    );
+    ellipse(
+      blocker[0] + blocker[1],
+      blocker[2] + 2 * blocker[1],
+      2 * blocker[1],
+      2 * blocker[1]
+    );
   }
 }
 
 function draw() {
-  // console.log(frameRate());
   clear();
-  background('rgba(0,0,40,1)');
+  background("rgba(0,0,40,1)");
   strokeWeight(rainWidth);
   drawRain();
   drawPerson();
   updateRain();
   updateObjects();
-
 }
